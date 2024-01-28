5d5d962ecb73c0de091f824ae9b292af50fd47bb
soma <soma@somas-MacBook-Pro.local>
2024-01-28 16:17:07 +0100

    updates

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Module with flask script"""
+"""siarts a Flask web application"""
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Method returns Hello HBNB"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a str"""
+    return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
